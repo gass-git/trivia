ad2f2835bb7bbbcdde3a64545f4e39807eb5f440
Gabriel Salinas <g.szada@gmail.com>
2022-02-06 11:19:00 +0100

    Removed web vitals package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.js'
-import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from "react-router-dom"
 import './custom.scss'
 
@@ -11,5 +10,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
-
-reportWebVitals();
