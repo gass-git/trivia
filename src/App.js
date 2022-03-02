53e74903d26b42dfd3a3142ca699abda5b3993f3
Gabriel Salinas <g.szada@gmail.com>
2022-03-02 11:17:26 +0100

    Exported the AppReducer state logic from the App component

--- src/App.js
+++ src/App.js
@@ -1,103 +1,16 @@
 import React, { useEffect, useReducer } from 'react';
 import { Routes, Route, Navigate } from "react-router-dom";
-import { specialChars } from "../src/data/specialChars.js";
 import GetData from './api/getData';
 import Quiz from './components/quiz';
 import Results from './components/results';
 import Home from './components/home';
+import AppReducer from './appReducer.js';
+import { initialState } from './appReducer.js';
 
 export const AppContext = React.createContext(null)
 
-function appReducer(state, action) {
-  const { data, answers, score, current, fetchErrorCount } = state
-  let [obj, question, newScore] = [{}, null, null]
-
-  switch (action.type) {
-    case 'API fetching error':
-      return {
-        ...state,
-        fetchErrorCount: fetchErrorCount + 1
-      }
-
-    case 'activate quiz':
-      return {
-        ...state,
-        isQuizActive: true
-      }
-
-    case 'activate results':
-      return {
-        ...state,
-        isResultsActive: true
-      }
-
-    case 'check answer':
-      question = data[current].question
-
-      // Fix question      
-      specialChars.forEach(special => {
-        question = question.replace(special.code, special.char)
-      })
-
-      obj.id = current
-      obj.question = question
-      obj.correctAnswer = data[current].correct_answer
-
-      if (action.answer === data[current].correct_answer) {
-        obj.isCorrect = true
-        newScore = score + 1
-      }
-      else {
-        obj.isCorrect = false
-        newScore = score
-      }
-
-      return {
-        ...state,
-        answers: [obj, ...answers],
-        score: newScore
-      }
-
-    case 'go to next question':
-      return {
-        ...state,
-        current: current + 1
-      }
-
-    case 'deactivate quiz':
-      return {
-        ...state,
-        isQuizActive: false
-      }
-
-    case 'update data':
-      return {
-        ...state,
-        data: action.data,
-        isFetchPending: false
-      }
-
-    case 'reset state':
-      return initialState
-
-    default:
-      return initialState
-  }
-}
-
-const initialState = {
-  data: null,
-  answers: [],
-  score: 0,
-  current: 0,
-  fetchErrorCount: 0,
-  isFetchPending: true,
-  isQuizActive: false,
-  isResultsActive: false
-}
-
 export default function App() {
-  const [state, dispatch] = useReducer(appReducer, initialState)
+  const [state, dispatch] = useReducer(AppReducer, initialState)
   const { fetchErrorCount, isFetchPending, isQuizActive, isResultsActive } = state
 
   useEffect(() => {
@@ -115,3 +28,5 @@ export default function App() {
     </AppContext.Provider>
   ]
 }
+
+export { initialState }
---
+++ src/appReducer.js
@@ -0,0 +1,89 @@
+import { specialChars } from "../src/data/specialChars.js";
+
+export const initialState = {
+  data: null,
+  answers: [],
+  score: 0,
+  current: 0,
+  fetchErrorCount: 0,
+  isFetchPending: true,
+  isQuizActive: false,
+  isResultsActive: false
+}
+
+export default function AppReducer(state, action) {
+  const { data, answers, score, current, fetchErrorCount } = state
+  let [obj, question, newScore] = [{}, null, null]
+
+  switch (action.type) {
+    case 'API fetching error':
+      return {
+        ...state,
+        fetchErrorCount: fetchErrorCount + 1
+      }
+
+    case 'activate quiz':
+      return {
+        ...state,
+        isQuizActive: true
+      }
+
+    case 'activate results':
+      return {
+        ...state,
+        isResultsActive: true
+      }
+
+    case 'check answer':
+      question = data[current].question
+
+      // Fix question      
+      specialChars.forEach(special => {
+        question = question.replace(special.code, special.char)
+      })
+
+      obj.id = current
+      obj.question = question
+      obj.correctAnswer = data[current].correct_answer
+
+      if (action.answer === data[current].correct_answer) {
+        obj.isCorrect = true
+        newScore = score + 1
+      }
+      else {
+        obj.isCorrect = false
+        newScore = score
+      }
+
+      return {
+        ...state,
+        answers: [...answers, obj],
+        score: newScore
+      }
+
+    case 'go to next question':
+      return {
+        ...state,
+        current: current + 1
+      }
+
+    case 'deactivate quiz':
+      return {
+        ...state,
+        isQuizActive: false
+      }
+
+    case 'update data':
+      return {
+        ...state,
+        data: action.data,
+        isFetchPending: false
+      }
+
+    case 'reset state':
+      return initialState
+
+    default:
+      return initialState
+  }
+}
